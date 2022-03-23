6bacca3d90321f906a01c64b66c97288f2f5f71c
mayank <Mayankr1777@gmail.com>
2022-03-23 12:12:09 +0530

    after creating popup for add task

--- index.js
+++ index.js
@@ -2,35 +2,89 @@
 
 const workOptions = document.getElementsByClassName("flex-list");
 
+let options = document.querySelectorAll(".draggable")
+
+var clickedId;
+
 //** iterate through all work options */
 
-for (option of workOptions) {
-
-  option.addEventListener("dragstart", (e) => {
-      console.log('dragstarted')
-    e.target.className += " hold";
-    setTimeout(() => {
-    }, 0);
+options.forEach (option =>  {
+  option.addEventListener("dragstart", (event) => {
+    console.log("dragstarted");
+    // e.target.className += " hold";
+    event.dataTransfer.setData("text", event.target.id);
+    console.log('data ' + event.dataTransfer.getData("text"))
+    setTimeout(() => {}, 0);
   });
 
-  option.addEventListener('dragend' , (e) => {
+  option.addEventListener("dragend", (e) => {
+    console.log("dragend");
     // e.target.className = 'option'
-})
+    e.target.classList.add("option")
+  });
+});
+
+let accepts = document.getElementsByClassName("accep");
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+  modal.classList.toggle("show-modal");
 }
 
-let accept = document.getElementById('accep');
-console.log(accept)
+function windowOnClick(event) {
+  if (event.target === modal) {
+    toggleModal();
+  }
+}
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
 
-accept.addEventListener('click' , () => {
-    alert('clicked!!')
+for (accept of accepts) {
+  accept.addEventListener("click", (e) => {
+    // alert('hello')
+    console.log("event is" + JSON.stringify(e.target));
+    console.log('event id is' + e.target.id)
+    this.clickedId = e.target.id
+    toggleModal();
+})
 
+    accept.addEventListener('dragover' , () => {
+
+    })
+    
+    accept.addEventListener('dragenter' , () => {
+    
+    })
+    
     accept.addEventListener('dragleave' , (e) => {
-        e.target.append(option)
+        e.preventDefault();
+        console.log('drag leave')
+        // e.target.append("flex-list")
+        // console.log('data is ' + e.dataTransfer.getData("text"))
+        e.target.classList.add("accep")
+        // console.log('value is '+ e.target.value)
     
     })
     
     accept.addEventListener('drop' , (e) => {
-        e.target.append(option)
-    
-    })
-})
+        e.preventDefault();
+        console.log('drag drop')
+        // e.target.append("flex-list")
+        e.target.classList.add("accep")
+        console.log('value is '+ e.target.value)
+  });
+
+
+}
+
+
+function addTask(e) {
+  let added = document.getElementById("task");
+//   alert(added.value + "added successfully");
+  let ids = document.getElementById(this.clickedId)
+  ids.innerHTML = added.value
+  ids.classList.add("flex-list")
+  toggleModal();
+}
